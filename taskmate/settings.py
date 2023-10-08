306b9df5f15ccf822ccbde611757244f1a4a2cbc
hackneychap <samuelbern@gmail.com>
2023-10-08 16:05:59 +0100

    maybe I've gone too far with these settings 	new file:   build_files.sh 	modified:   taskmate/settings.py 	modified:   vercel.json

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles",'staticfiles_build',"static")
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = "todolist"
